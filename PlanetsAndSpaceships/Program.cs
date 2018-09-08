6c55e7d18d3be795a7ea12db59e7c605d41efd73
Andy Million <amillion3@gmail.com>
2018-09-08 12:48:21 -0500

    initial commit

--- PlanetsAndSpaceships/Program.cs
+++ PlanetsAndSpaceships/Program.cs
@@ -6,7 +6,28 @@ namespace PlanetsAndSpaceships
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+
+            //            In the Main method, place the following code
+
+            //List<string> planetList = new List<string>() { "Mercury", "Mars" };
+            //            Add() Jupiter and Saturn at the end of the list.
+            //Create another List that contains that last two planet of our solar system.
+            //Combine the two lists by using AddRange().
+            //Use Insert() to add Earth, and Venus in the correct order.
+            //Use Add() again to add Pluto to the end of the list.
+            //Now that all the planets are in the list, slice the list using GetRange() in order to extract the rocky planets into a new list called rockyPlanets.
+            //Being good amateur astronomers, we know that Pluto is now a dwarf planet, so use the Remove() method to eliminate it from the end of planetList.
+            //Iterating over planets
+            //Ref: List of Solar System probes
+
+            //Create another dictionary that will hold the name of a spacecraft that we have launched, and a list of names of the planets that it has visited.Remember that List is a Type just like native types(such as string, int, &bool) and your custom types(such as Movie, Dog, and Food). They can be passed to anything, like a dictionary, as a Type.
+            //Iterate over your list of planets from above, and inside that loop, iterate over the dictionary. Write to the console, for each planet, which satellites have visited which planet.
+            //Mars: Viking, Opportunity, Curiosity
+            //Venus: Mariner, Venera
+
+            //etc...
         }
     }
 }
